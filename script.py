d4e1e8cf4f6839d61363ff01bbbbdfcca5a6b18e
Drrrk Ronin <a.dark.ronin@gmail.com>
2022-12-11 03:25:01 +0100

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,6 @@
 import tweepy # for tweeting
 import time # for sleeping
 import datetime # for setting the date and time of the first tweet
-from threading import Thread # for using a thread
 
 # Set the date and time of the first tweet
 first_tweet_date = datetime.datetime(2022, 12, 11, 9, 30) # December 11, 2022 at 9:30am
@@ -16,7 +15,12 @@ access_token_secret = 'your_access_token_secret'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+try:
+    api = tweepy.API(auth)
+except tweepy.error.TweepError as e:
+    # Handle any errors that may be raised when trying to authenticate with the Twitter API
+    print(e)
+    exit(1)
 
 # Set the text of the tweets in a list
 tweet_text = ['Hello, world!', 'This is my second tweet.', 'And this is my third tweet.']
@@ -25,15 +29,22 @@ tweet_text = ['Hello, world!', 'This is my second tweet.', 'And this is my third
 end_date = first_tweet_date + datetime.timedelta(weeks=12) # 3 months is approximately 12 weeks
 
 # Tweet each tweet in the list
-for i, tweet in enumerate(tweet_text):
-    # Create a thread to tweet the current tweet
-    thread = Thread(target=tweet_tweet, args=(tweet,))
-    thread.start()
+for tweet in tweet_text:
+    # Wait until the current time is greater than or equal to the first tweet date
+    while datetime.datetime.now() < first_tweet_date:
+        time.sleep(1)
+
+    # Tweet the current tweet
+    try:
+        api.update_status(tweet)
+    except tweepy.error.TweepError as e:
+        # Handle any errors that may be raised by the Twitter API
+        print(e)
+        continue
 
     # Sleep for 24 hours
     time.sleep(24 * 60 * 60)
 
-def tweet_tweet(tweet):
-    while datetime.datetime.now() < end_date:
-        if datetime.datetime.now() >= first_tweet_date:
-            api.update_status(tweet) # tweet
+    # Check if the end date has been reached
+    if datetime.datetime.now() >= end_date:
+        break
