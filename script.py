bba39fb9f02a648329e50eceb36a360691ccbd64
Drrrk Ronin <a.dark.ronin@gmail.com>
2022-12-11 03:03:22 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,39 @@
+# Import the necessary libraries
+1 import tweepy # for tweeting
+2 import time # for sleeping
+3 import datetime # for setting the date and time of the first tweet
 
+# Set the date and time of the first tweet
+4 first_tweet_date = datetime.datetime(2022, 12, 11, 9, 30) # December 11, 2022 at 9:30am
+
+# Set up the Twitter API with your own credentials
+5 consumer_key = 'your_consumer_key'
+6 consumer_secret = 'your_consumer_secret'
+7 access_token = 'your_access_token'
+8 access_token_secret = 'your_access_token_secret'
+9 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+10 auth.set_access_token(access_token, access_token_secret)
+11 api = tweepy.API(auth)
+
+# Set the text of the tweets in a list
+12 tweet_text = ['Hello, world!', 'This is my second tweet.', 'And this is my third tweet.']
+
+# Tweet automatically every 24 hours for 3 months
+13 end_date = first_tweet_date + datetime.timedelta(weeks=12) # 3 months is approximately 12 weeks
+
+# Keep track of which tweet we are currently on
+14 current_tweet_index = 0
+
+15 while datetime.datetime.now() < end_date:
+16     if datetime.datetime.now() >= first_tweet_date:
+17         # Tweet the current tweet in the list
+18         api.update_status(tweet_text[current_tweet_index])
+
+19         # Go to the next tweet in the list
+20         current_tweet_index += 1
+
+21         # If we have reached the end of the list, go back to the beginning
+22         if current_tweet_index >= len(tweet_text):
+23             current_tweet_index = 0
+
+24     time.sleep(24 * 60 * 60) # sleep for 24 hours
