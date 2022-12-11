955ddb936245096a98ebd7a1135034f4a37070bd
Drrrk Ronin <a.dark.ronin@gmail.com>
2022-12-11 04:08:31 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,15 @@
 # Import the necessary libraries
 import tweepy # for tweeting
 import time # for sleeping
-import datetime # for setting the date and time of the first tweet
+from threading import Thread # for using a thread pool
+from datetime import datetime, timedelta # for setting the date and time of the first tweet
+from queue import Queue # for storing the tweets in a queue
+from tweepy import API # for using a Twitter API client
+from tweepy.error import TweepError # for handling Twitter API errors
+from tweepy import Cursor # for iterating over Twitter API results
 
 # Set the date and time of the first tweet
-first_tweet_date = datetime.datetime(2022, 12, 11, 9, 30) # December 11, 2022 at 9:30am
+first_tweet_date = datetime(2022, 12, 11, 9, 30) # December 11, 2022 at 9:30am
 
 # Set up the Twitter API with your own credentials
 consumer_key = 'your_consumer_key'
@@ -12,39 +17,55 @@ consumer_secret = 'your_consumer_secret'
 access_token = 'your_access_token'
 access_token_secret = 'your_access_token_secret'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-try:
-    api = tweepy.API(auth)
-except tweepy.error.TweepError as e:
-    # Handle any errors that may be raised when trying to authenticate with the Twitter API
-    print(e)
-    exit(1)
-
 # Set the text of the tweets in a list
 tweet_text = ['Hello, world!', 'This is my second tweet.', 'And this is my third tweet.']
 
+# Create a thread pool to tweet the messages
+num_threads = 10 # number of threads in the pool
+tweet_queue = Queue() # queue for storing the tweets
+
+# Create the threads in the thread pool
+for i in range(num_threads):
+    thread = Thread(target=tweet_tweet)
+    thread.start()
+
 # Tweet automatically every 24 hours for 3 months
-end_date = first_tweet_date + datetime.timedelta(weeks=12) # 3 months is approximately 12 weeks
+end_date = first_tweet_date + timedelta(weeks=12) # 3 months is approximately 12 weeks
+
+# Set up the Twitter API client
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Get the list of tweets that have already been sent
+sent_tweets = []
+try:
+    # Use the Twitter API to get the list of tweets that have been sent
+    for status in Cursor(api.user_timeline).items():
+        sent_tweets.append(status.text)
+except TweepError as e:
+    # Handle any errors that may be raised by the Twitter API
+    print(e)
 
 # Tweet each tweet in the list
 for tweet in tweet_text:
     # Wait until the current time is greater than or equal to the first tweet date
-    while datetime.datetime.now() < first_tweet_date:
+    while datetime.now() < first_tweet_date:
         time.sleep(1)
 
-    # Tweet the current tweet
-    try:
-        api.update_status(tweet)
-    except tweepy.error.TweepError as e:
-        # Handle any errors that may be raised by the Twitter API
-        print(e)
-        continue
+    # Check if the tweet has already been sent
+    if tweet not in sent_tweets:
+        # Add the tweet to the queue
+        tweet_queue.put(tweet)
 
-    # Sleep for 24 hours
-    time.sleep(24 * 60 * 60)
+# Wait until all of the tweets have been sent
+tweet_queue.join()
 
-    # Check if the end date has been reached
-    if datetime.datetime.now() >= end_date:
-        break
+def tweet_tweet():
+    # Set up the Twitter API client
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    while True:
+        # Get the next tweet from the queue
